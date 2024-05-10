53d4cbc254e83790f1ff0861b45008b234b025ef
cph718 <cph718@gmail.com>
2024-05-10 22:09:36 +0800

    Got Path0 Setup and quick run working. Tested E-Stop

--- Testing/TestMBServo.py
+++ Testing/TestMBServo.py
@@ -1,7 +1,7 @@
-
+#Code to test Servo can be controlled
 
 #import serial
-#import time
+import time
 import pymodbus.client as ModbusClient
 from pymodbus import (
     ExceptionResponse,
@@ -18,7 +18,7 @@ def run_sync_simple_client():
 
     print("get and verify data")
     try:
-        rr = client.read_coils(1, 1, slave=1)
+        rr = client.read_holding_registers(9, 2, slave=1)
     except ModbusException as exc:
         print(f"Received ModbusException({exc}) from library")
         client.close()
@@ -31,6 +31,51 @@ def run_sync_simple_client():
         print(f"Received Modbus library exception ({rr})")
         # THIS IS NOT A PYTHON EXCEPTION, but a valid modbus message
         client.close()
+
+    client.write_register(0x0003, 0x0, slave=1) # Set to PR Mode
+    print("PR Mode")
+    client.write_register(0x0405, 0x83, slave=1) # Enable Servo Motor
+    print("Enable Servo")
+
+    #Testing Writing Registers
+    #rr = client.read_holding_registers(9, 2, slave=1)
+    #print(rr.registers)
+    #client.write_register(9, 250, slave=1)
+    #rr = client.read_holding_registers(9, 2, slave=1)
+    #print(rr.registers)
+    #rr = client.read_holding_registers(9, 2, slave=1)
+
+    #Test Run Path0.
+    #rr = client.read_holding_registers(0x0405, 2, slave=1)
+    #print(rr.registers)
+    #rr = client.read_holding_registers(0x6002, 2, slave=1)
+    #print(rr.registers)
+    #client.write_register(0x6002, 0x10, slave=1)
+    #rr = client.read_holding_registers(0x6002, 2, slave=1)
+    #print(rr.registers)
+
+    #Test Jog Mode
+    rr = client.read_holding_registers(0x6200, 16, slave=1)
+    print(rr.registers)
+    packetData = [0x0002, 0x0000, 0x0000, 0x03e8, 0x0062, 0x0060, 0x0000, 0x0010]
+    client.write_registers(0x6200, packetData, slave=1)
+    rr = client.read_holding_registers(0x6200, 16, slave=1)
+    print(rr.registers)
+
+    #i = 0
+    #while i < 10:
+    #    client.write_register(0x6207, 0x10, slave=1)
+    #    print(i)
+    #    i += 1
+
+    time.sleep(10)
+
+    client.write_register(0x6002, 0x0040, slave=1) # E-Stop
+    print("E-Stop")
+
+    time.sleep(2)
+
+    client.write_register(0x0405, 0x3, slave=1) # Disable Servo Motor
 
     print("close connection")
     client.close()
